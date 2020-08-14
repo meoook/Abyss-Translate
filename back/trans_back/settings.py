89fd0b631a2c8f5d0e211d0bacec048c56923c89
meoook <mewooook@gmail.com>
2020-08-15 00:46:10 +0300

    Celery + redis

--- back/trans_back/settings.py
+++ back/trans_back/settings.py
@@ -4,7 +4,7 @@ from django.core.files.storage import FileSystemStorage
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "NO_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
