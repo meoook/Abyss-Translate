06b3e73e0cbd9584a9cc058d528d854577b4dde5
meok-mac <mewooook@gmail.com>
2020-08-16 19:36:32 +0300

    tasks git

--- back/core/tasks.py
+++ back/core/tasks.py
@@ -24,8 +24,33 @@ def file_parse(filo):
     return True
 
 
+@shared_task(
+    name="Update file from git repository",
+    max_retries=2,
+    soft_time_limit=1,
+    time_limit=5,
+    rate_limit='1/h',
+    ignore_result=True
+)
+def file_update_from_repo(filo):
+    """ After adding new file by user -> Check (exist & access) and update file in repository folder files """
+    return True
+
+
+@shared_task(
+    name="Update files in git repository folder",
+    max_retries=1,
+    soft_time_limit=1,
+    time_limit=5,
+    rate_limit='4950/h',
+    ignore_result=True
+)
+def folder_update_by_repo(filo):
+    """ After changing git url -> Update folder files in git repository folder """
+    return True
+
 @periodic_task(
-    name="Check users files",
+    name="Check users files in git repositories",
     run_every=crontab(hour='*/1'),
     max_retries=0,
     soft_time_limit=30,
@@ -35,12 +60,31 @@ def file_parse(filo):
     store_errors_even_if_ignored=True
 )
 def check_all_file_repos():
-    """ It's a global app task. Get all users files that have repository, and check them for update. """
+    """ It's a global app task -> Get all users files that have git repo and update if needed """
     try:
         pass
     except SoftTimeLimitExceeded:
         logger.warning('Checking files too slow')
         pass
+
+@periodic_task(
+    name="Upload translated files into git repositories",
+    run_every=crontab(hour='*/1'),
+    max_retries=0,
+    soft_time_limit=30,
+    time_limit=59,
+    rate_limit='1/h',
+    ignore_result=True,
+    store_errors_even_if_ignored=True
+)
+def check_all_file_repos():
+    """ It's a global app task -> Get all users translated files and upload them in git repos """
+    try:
+        pass
+    except SoftTimeLimitExceeded:
+        logger.warning('Checking files too slow')
+        pass
+
 
 
 @periodic_task(run_every=timedelta(seconds=10), name="Task to check celery. Run each 10 seconds.")
--- back/core/utils/utils.py
+++ back/core/utils/utils.py
@@ -1,6 +1,15 @@
 import hashlib
 import re
 
+def get_md5_f(binary_data, clear=False):
+    """ If clear - also return md5 of cleaned text to search same text """
+    md5sum = hashlib.md5(binary_data).hexdigest()
+    if clear:
+        re_clean = r'[\s\d\.\"\'\\\.\,\>\<\-\:\;\!\?\@\#\$\%\^\&\*\(\)\+\=\/\|\_\[\]\{\}]'
+        clean = re.sub(re_clean.encode(), b'', binary_data)
+        md5sum_only_letters = hashlib.md5(clean).hexdigest()
+        return md5sum, md5sum_only_letters
+    return md5sum
 
 def get_md5(binary_data, codec=None):
     """ If codec set - also return md5 of cleaned text to search same text """
@@ -15,9 +24,10 @@ def get_md5(binary_data, codec=None):
 
 def count_words(text):
     """ App function to find number of words(payment) in text """
+    word_min_len_to_count = 2
     re_clean = r'[\d\.\"\'\\\.\,\>\<\-\:\;\!\?\@\#\$\%\^\&\*\(\)\+\=\/\|\_\[\]\{\}]'
     clean = re.sub(re_clean, '', text)
-    return len([x for x in clean.split(' ') if len(x) > 2])
+    return len([x for x in clean.split(' ') if len(x) > word_min_len_to_count])
 
 
 def csv_validate_text(text):
