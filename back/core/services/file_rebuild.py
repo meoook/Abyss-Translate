07720e53be3478dbc573d35455aa7a83718b26c0
meoook <mewooook@gmail.com>
2020-08-17 22:38:52 +0300

    rebuild

--- back/core/services/file_rebuild.py
+++ back/core/services/file_rebuild.py
@@ -2,25 +2,25 @@ import re
 
 from django.db.models import Sum, Count
 
-from core.models import Files, FileMarks, Translates, Translated
+from core.models import FileMarks, Translates
 from core.utils.utils import get_md5, count_words, csv_validate_text
 
 
 class FileRebuild:
     """ Rebuild translates after file update """
-    def __init__(self, f_path, codec, f_name, file_id, f_lang_id, f_method, f_options):
+    def __init__(self, f_path, codec, _, file_id, f_lang_id, f_method, f_options):
         self.__marks_to_check = {}
         self.__marks_to_update = []
         self.__marks_to_add = []
         self.stats = {}
         self.error = None
         # Check params
-        if not isinstance(file_id, int) or not isinstance(lang_orig_id, int) or not f_method or not f_path:
+        if not isinstance(file_id, int) or not isinstance(f_lang_id, int) or not f_method or not f_path:
             self.error = 'Wrong arguments'
         else:
-        	self.__init_build(file_id, f_method, f_path, codec, f_lang_id, f_options)
+            self.__start_build(file_id, f_method, f_path, codec, f_lang_id, f_options)
 
-    def __itit_build(self, file_id, method, file_path, codec, lang_orig_id, options):
+    def __start_build(self, file_id, method, file_path, codec, lang_orig_id, options):
         # Get file marks if file was before
         for x in FileMarks.objects.filter(file_id=file_id):
             self.__marks_to_check[x.id] = {
@@ -33,68 +33,68 @@ class FileRebuild:
         rebuild = len(self.__marks_to_check) == 0
         # Get self.__marks_to_*** arrays
         with open(file_path, 'rb') as filo:
-			if method == 'ue':
-				self.parse_as_ue(filo, codec, options)
-			elif method == 'csv':
-				self.parse_as_csv(filo, codec, options)
-			elif method == 'html':
-				self.parse_as_html(filo, codec, options)
-			else:
-				self.error = f"Wrong method {method} for file {file_name}{file_id}")
-				return False
+            if method == 'ue':
+                self.parse_as_ue(filo, codec, options)
+            elif method == 'csv':
+                self.parse_as_csv(filo, codec, options)
+            elif method == 'html':
+                self.parse_as_html(filo, codec)
+            else:
+                self.error = f"Wrong method {method}"
+                return False
         self.__build_marks(file_id, lang_orig_id, rebuild)
         # Get file stats
         stats = FileMarks.objects.filter(file_id=file_id).aggregate(items_count=Count('id'), total_words=Sum('words'))
         self.stats['words'] = stats['total_words']
         self.stats['items'] = stats['items_count']
-		return True
+        return True
 
     def parse_as_ue(self, filo, codec, f_options):
-		# Pass header
-		if 'top_rows' in f_options and f_options['top_rows']:
-			for i, _ in enumerate(filo):
-				if i == f_options['top_rows'] - 1:
-					break
-		# Parsing file
-		object_number = 0
-		while True:
-			try:
-				object_number += 1
-				trans_object = [next(filo) for _ in range(7)]
-			except StopIteration:
-				break
-			text = trans_object[5].decode(codec)[8:-3]     # FIXME [8:-3] -> re ^msgstr\ \"(.*)\"$
-			if len(text.strip()) == 0:
-				continue
-			options = trans_object[3].decode(codec)[9:-3]
-			self.__check_or_create_mark(object_number, None, text, codec, options)
+        # Pass header
+        if 'top_rows' in f_options and f_options['top_rows']:
+            for i, _ in enumerate(filo):
+                if i == f_options['top_rows'] - 1:
+                    break
+        # Parsing file
+        object_number = 0
+        while True:
+            try:
+                object_number += 1
+                trans_object = [next(filo) for _ in range(7)]
+            except StopIteration:
+                break
+            text = trans_object[5].decode(codec)[8:-3]     # FIXME [8:-3] -> re ^msgstr\ \"(.*)\"$
+            if len(text.strip()) == 0:
+                continue
+            options = trans_object[3].decode(codec)[9:-3]
+            self.__check_or_create_mark(object_number, None, text, codec, options)
 
     def parse_as_csv(self, filo, codec, options):
-		quotes_l = int(options['quotes'][0]) if 'quotes' in options else 0
-		quotes_r = int(options['quotes'][1]) if 'quotes' in options else 0
-		delimiter = options['delimiter']
-		fields = options['fields']
-		object_number = 0
-		for i, row in enumerate(filo):
-			if 'top_rows' in options and options['top_rows'] > i:
-				continue
-			object_number += 1
-			for idx, text in enumerate(row.decode(codec).split(delimiter)):
-				if idx in fields:
-					text = csv_validate_text(text)
-					if text is False or len(text) <= quotes_l + quotes_r:
-						continue
-					text = text[quotes_l:-quotes_r] if quotes_r > 0 else text[quotes_l:]
-					self.__check_or_create_mark(object_number, idx, text, codec)
+        quotes_l = int(options['quotes'][0]) if 'quotes' in options else 0
+        quotes_r = int(options['quotes'][1]) if 'quotes' in options else 0
+        delimiter = options['delimiter']
+        fields = options['fields']
+        object_number = 0
+        for i, row in enumerate(filo):
+            if 'top_rows' in options and options['top_rows'] > i:
+                continue
+            object_number += 1
+            for idx, text in enumerate(row.decode(codec).split(delimiter)):
+                if idx in fields:
+                    text = csv_validate_text(text)
+                    if text is False or len(text) <= quotes_l + quotes_r:
+                        continue
+                    text = text[quotes_l:-quotes_r] if quotes_r > 0 else text[quotes_l:]
+                    self.__check_or_create_mark(object_number, idx, text, codec)
 
-    def parse_as_html(self, filo, codec, options):
-		data = filo.read().decode(codec)
-		for idx, re_obj in enumerate(re.finditer(r'\>\s*([^\<\>]+[^\s])\s*\<', data)):
-			text = re_obj.group(1)
-			options = {'coordinates': re_obj.span(1)}
-			if len(text.strip()) == 0:
-				continue
-			self.__check_or_create_mark(idx, None, text, codec, options)
+    def parse_as_html(self, filo, codec):
+        data = filo.read().decode(codec)
+        for idx, re_obj in enumerate(re.finditer(r'>\s*([^<>]+[^\s])\s*<', data)):
+            text = re_obj.group(1)
+            options = {'coordinates': re_obj.span(1)}
+            if len(text.strip()) == 0:
+                continue
+            self.__check_or_create_mark(idx, None, text, codec, options)
 
     def __check_or_create_mark(self, mark_n, col_n, text, codec, options=None):
         """ Find what array mark to add (leave to remove, update, create) """
@@ -128,7 +128,8 @@ class FileRebuild:
         # Delete/Update mark
         if rebuild:
             FileMarks.objects.filter(id__in=self.__marks_to_check.keys()).delete()
-            for mark_item in sorted(self.__marks_to_update, key=lambda m: m['new_number'], reverse=True):   # TODO: sort by col
+            for mark_item in sorted(self.__marks_to_update, key=lambda m: m['new_number'], reverse=True):
+                # TODO: sort by col
                 FileMarks.objects.filter(id=mark_item['id'])\
                     .update(mark_number=mark_item['new_number'], col_number=mark_item['col_number'])
         # Create new mark and original translate
---
+++ back/core/services/utils.py
@@ -0,0 +1,38 @@
+import hashlib
+import re
+
+
+def get_md5_f(binary_data, clear=False):
+    """ If clear - also return md5 of cleaned text to search same text """
+    md5sum = hashlib.md5(binary_data).hexdigest()
+    if clear:
+        pattern = r'[\s\d\-\/\[\]\"\'\\`~.,><:;!?@#$%^&*()+=|_{}]'.encode()
+        clean = re.sub(pattern, b'', binary_data)
+        md5sum_only_letters = hashlib.md5(clean).hexdigest()
+        return md5sum, md5sum_only_letters
+    return md5sum
+
+
+def get_md5(binary_data, codec=None):
+    """ If codec set - also return md5 of cleaned text to search same text """
+    md5sum = hashlib.md5(binary_data).hexdigest()
+    if codec:
+        clean = re.sub(r'[\s\d\-\/\[\]\"\'\\`~.,><:;!?@#$%^&*()+=|_{}]', '', binary_data.decode(codec))
+        md5sum_only_letters = hashlib.md5(clean.encode(codec)).hexdigest()
+        return md5sum, md5sum_only_letters
+    return md5sum
+
+
+def count_words(text):
+    """ App function to find number of words(payment) in text """
+    word_min_len_to_count = 2
+    clean = re.sub(r'[\d\-\/\[\]\"\'\\`~.,><:;!?@#$%^&*()+=|_{}]', '', text)
+    return len([x for x in clean.split(' ') if len(x) > word_min_len_to_count])
+
+
+def csv_validate_text(text):
+    # if re.match('^([N|n]one|[F|f]alse|[0-9]*\,+[0-9]*|[0-9\s]*|([^\s]*[\.\_][^\s]+)+)$', text):
+    text = text.strip()
+    if re.match(r'^([N|n]one|[F|f]alse|[T|t]rue|[0-9]*,+[0-9]*|[0-9\s]*|[^\s]*[._][^\s]+)$', text):
+        return False
+    return text
