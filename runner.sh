b57917d64a75c42506ca87f8286ce22e58b7e9c4
meok-mac <mewooook@gmail.com>
2020-08-18 04:03:05 +0300

    run + md

--- runner.sh
+++ runner.sh
@@ -1,52 +1,55 @@
 #!/bin/bash
+MYCUSTOMTAB='   '
+echo "====================================="
+echo "${MYCUSTOMTAB}-= Abyss localize manager =-"
 
-if [ "$#" -ne 1 ]; then
-    echo "No param or empty"
-    exit 1
+if [ "$#" -eq 1 ]; then
+    echo "====================================="
+    echo "${MYCUSTOMTAB}Option selected: $1"
+    selected=$1
+elif [ "$#" -eq 2 ] && [ $1 -eq 3 ]; then
+    echo "====================================="
+    echo "${MYCUSTOMTAB}Display logs..."
+    echo "====================================="
+    selected=3
+    selected2=$2
 else
-    echo "Param is $1"
-    exit 1
-fi
-
-if [ -z "$1" ]
-then
-    echo "================================"
-    echo "1 - make migrations and migrate"
-    echo "2 - create superuser"
-    echo "3 - display logs"
-    echo "0 - server and volumes down"
-    echo "default - rebuild"
-    echo "================================"
-
+    echo "====================================="
+    echo "${MYCUSTOMTAB}1 - make migrations and migrate"
+    echo "${MYCUSTOMTAB}2 - create superuser"
+    echo "${MYCUSTOMTAB}3 - display logs"
+    echo "${MYCUSTOMTAB}5 - build and run server"
+    echo "${MYCUSTOMTAB}0 - down server and volumes"
+    echo "${MYCUSTOMTAB}* - rebuild"
+    echo "====================================="
     read -p "Select option to do: " option
     selected=$option
-else
-    echo "\$1 not empty"
-    selected=$1
 fi
 
 case "$selected" in
     1) docker-compose run --rm django sh -c "python manage.py makemigrations"
        docker-compose run --rm django sh -c "python manage.py migrate" ;;
     2) docker-compose run --rm django sh -c "python manage.py createsuperuser" ;;
-    3) 
-        echo "================================"
-        echo "1 - react"
-        echo "2 - celery"
-        echo "3 - redis"
-        echo "4 - postgres"
-        echo "default - django"
-        echo "================================"
-        read -p "Select srv to display logs for: " log_option
+    3)  if [ -z "$selected2" ]; then
+            echo "====================================="
+            echo "${MYCUSTOMTAB}1 - react"
+            echo "${MYCUSTOMTAB}2 - celery"
+            echo "${MYCUSTOMTAB}3 - redis"
+            echo "${MYCUSTOMTAB}4 - postgres"
+            echo "${MYCUSTOMTAB}* - django"
+            echo "====================================="
+            read -p "Select srv to display logs for: " log_option
+            selected2=$log_option
+        fi
 
-        case "$log_option" in
+        case "$selected2" in
             1) docker-compose logs react ;;
             2) docker-compose logs celery ;;
             3) docker-compose logs redis ;;
             3) docker-compose logs postgres ;;
             *) docker-compose logs django ;;
-        esac
-     ;;
+        esac ;;
+    5) docker-compose up -d --build ;;
     0) docker-compose down -v ;;
     *) docker-compose down && docker-compose up -d --build ;;
 esac
