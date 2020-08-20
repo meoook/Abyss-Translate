6c78d9035d4b7de3cdd514b0e49a136f466d2e4d
meoook <meoook@gmail.com>
2020-08-20 13:54:45 -0700

    k

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """ VIP status and user config for UI  """
 
-    class Meta:
-        permissions = [
-            ("creator", "Can create projects and invite"),
-            ("admin", "Can manage projects where invited"),
-            ("translator", "Can translate files from projects where invited"),
-        ]
+    # class Meta:
+    #     permissions = [
+    #         ("creator", "Can create projects and invite"),
+    #         ("admin", "Can manage projects where invited"),
+    #         ("translator", "Can translate files from projects where invited"),
+    #     ]
 
     vip_status = models.BooleanField(default=False)
     config = models.JSONField(null=True)
