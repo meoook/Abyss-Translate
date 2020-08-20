c9fd467713c7e61686db5d3e5f14c7037303ada9
meoook <mewooook@gmail.com>
2020-08-20 23:39:40 +0300

    dfs

--- back/core/admin.py
+++ back/core/admin.py
@@ -1,97 +1,52 @@
 from django.contrib import admin
-from .models import Languages, Translates
+from .models import Languages
 
 from django import forms
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.core.exceptions import ValidationError
-
-from django.conf import settings
 
 
-class LanguagesAdmin(admin.ModelAdmin):
-    pass
-
-
-class UserCreationForm(forms.ModelForm):
+@admin.register(Languages)
+class AddLanguagesAdmin(admin.ModelAdmin):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    name = forms.CharField(label='Name', widget=forms.TextInput)
+    short_name = forms.CharField(label='Short name', widget=forms.TextInput)
+    active = forms.BooleanField(label='Active', widget=forms.CheckboxInput)
 
     class Meta:
-        model = settings.AUTH_USER_MODEL
-        fields = ('email', 'date_of_birth')
-
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords don't match")
-        return password2
+        model = Languages
+        fields = ('name', 'short_name', 'active')
 
     def save(self, commit=True):
         # Save the provided password in hashed format
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        language = super().save(commit=False)
+        language.set_password(self.cleaned_data["password1"])
         if commit:
-            user.save()
-        return user
+            language.save()
+        return language
 
 
-class UserChangeForm(forms.ModelForm):
-    """A form for updating users. Includes all the fields on
-    the user, but replaces the password field with admin's
-    password hash display field.
-    """
-    password = ReadOnlyPasswordHashField()
-
-    class Meta:
-        model = settings.AUTH_USER_MODEL
-        fields = ('email', 'password', 'date_of_birth', 'is_active', 'is_admin')
-
-    def clean_password(self):
-        # Regardless of what the user provides, return the initial value.
-        # This is done here, rather than on the field, because the
-        # field does not have access to the initial value
-        return self.initial["password"]
-
-
-class UserAdmin(BaseUserAdmin):
+class LanguagesAdmin(admin.ModelAdmin):
     # The forms to add and change user instances
-    form = UserChangeForm
-    add_form = UserCreationForm
+    # form = UserChangeForm
+    add_form = AddLanguagesAdmin
 
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'date_of_birth', 'is_admin')
-    list_filter = ('is_admin',)
+    list_display = ('name', 'short_name', 'active')
+    list_filter = ('name',)
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {'fields': ('date_of_birth',)}),
-        ('Permissions', {'fields': ('is_admin',)}),
+        (None, {'fields': ('name', 'short_name', 'active')}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
     # overrides get_fieldsets to use this attribute when creating a user.
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'date_of_birth', 'password1', 'password2'),
+            'fields': ('name', 'short_name', 'active'),
         }),
     )
-    search_fields = ('email',)
-    ordering = ('email',)
+    search_fields = ('name',)
+    ordering = ('name',)
     filter_horizontal = ()
-
-
-# Now register the new UserAdmin...
-admin.site.register(settings.AUTH_USER_MODEL, UserAdmin)
-# ... and, since we're not using Django's built-in permissions,
-# unregister the Group model from admin.
-admin.site.unregister(Group)
-
-admin.site.register(Languages)
-admin.site.register(Translates)
