10fdb797e20f983aad6ec11222708ed195a705c5
meoook <meoook@gmail.com>
2020-08-20 20:39:20 -0700

    sad

---
+++ back/core/tests/test_gogo.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+
+class ProjectsSerializerTestCase(TestCase):
+
+    def test_for_the_test(self):
+        """ Some errors with django """
+        self.assertEqual('a', 'b')
+
+    def test_for_the_test2(self):
+        """ Some errors with django222 """
+        self.assertEqual('az', 'bc')
+
+    def test_for_the_test3(self):
+        """ Some errors with django222 """
+        self.assertEqual('bcc', 'bcc')
