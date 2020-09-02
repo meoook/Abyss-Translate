9c324360ebe74f4db00e2d4405bd073171ecc975
meok-mac <mewooook@gmail.com>
2020-09-02 07:24:35 +0300

    ook

--- front/src/Elems/PageProject/FileListByAccess.js
+++ front/src/Elems/PageProject/FileListByAccess.js
@@ -1,87 +1,94 @@
-import React, { useEffect, useContext, useState } from "react"
-import AppContext from "../../context/application/appContext"
-import { IcoGet } from "../icons"
-import Loader from "../AppComponents/Loader"
-import Paginator from "../AppComponents/Paginator"
-import FileTranslatedStatus from "../AppComponents/FileTranslatedStatus"
-import { Link } from "react-router-dom"
-import { textCutByLen } from "../componentUtils"
+import React, { useEffect, useContext, useState } from 'react';
+import AppContext from '../../context/application/appContext';
+import { IcoGet } from '../icons';
+import Loader from '../AppComponents/Loader';
+import Paginator from '../AppComponents/Paginator';
+import FileTranslatedStatus from '../AppComponents/FileTranslatedStatus';
+import { Link } from 'react-router-dom';
+import { textCutByLen } from '../componentUtils';
 
 const FileListByAccess = ({ trOnly, fldrID, prjID }) => {
-  const [loading, setLoading] = useState(true)
-  const { explorer, explList } = useContext(AppContext)
-  const [fileList, setFileList] = useState([])
-  const [page, setPage] = useState(1)
-  const [size, setSize] = useState(25)
+  const [loading, setLoading] = useState(true);
+  const { explorer, explList } = useContext(AppContext);
+  const [fileList, setFileList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(25);
 
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     if (trOnly) {
-      explList(prjID, null, page, size).then(() => {})
+      explList(prjID, null, page, size);
     } else if (fldrID) {
-      explList(prjID, fldrID, page, size).then(() => {})
+      explList(prjID, fldrID, page, size);
     } else {
-      explList(null, null)
+      explList(null, null);
     }
     // eslint-disable-next-line
-  }, [fldrID, page, size])
+  }, [fldrID, page, size]);
 
   useEffect(() => {
     if (explorer.results) {
-      setFileList(explorer.results)
+      setFileList(explorer.results);
     } else {
-      setFileList([])
+      setFileList([]);
     }
-    setLoading(false)
-  }, [explorer])
+    setLoading(false);
+  }, [explorer]);
 
   const refreshPage = (pageNumber, pageSize) => {
-    setLoading(true)
-    setPage(pageNumber)
-    setSize(pageSize)
+    setLoading(true);
+    setPage(pageNumber);
+    setSize(pageSize);
     if (trOnly)
       explList(prjID, null, page, size).then(() => {
-        setLoading(false)
-      })
+        setLoading(false);
+      });
     else if (fldrID) {
       explList(prjID, fldrID, pageNumber, pageSize).then(() => {
-        setLoading(false)
-      })
+        setLoading(false);
+      });
     }
-  }
+  };
 
   return (
     <div className={`col col-${trOnly ? 12 : 9}`}>
-      <div className={`table-head${!trOnly ? " ml-3" : ""}`}>
+      <div className={`table-head${!trOnly ? ' ml-3' : ''}`}>
         <div className='col col-3'>Название</div>
         <div className='col col-2'>Позиций</div>
         <div className='col col-2'>Слов</div>
         <div className='col col-3'>Прогресс перевода</div>
         <div className='col col-2'>GIT статус</div>
       </div>
-      <div className={`scroll-y paginate column m-1${!trOnly ? " ml-3" : ""}`}>
+      <div className={`scroll-y paginate column m-1${!trOnly ? ' ml-3' : ''}`}>
         {loading ? (
           <Loader />
         ) : !fileList.length ? (
-          <div className='col col-4'>{trOnly ? <NoFilesList /> : fldrID ? <NoFilesInfo /> : <></>}</div>
+          <div className='col col-4'>
+            {trOnly ? <NoFilesList /> : fldrID ? <NoFilesInfo /> : <></>}
+          </div>
         ) : (
           <>
             {fileList.map((item) => (
               <FileItem file={item} key={item.id} />
             ))}
-            <Paginator page={page} size={size} total={explorer.count} refresh={refreshPage} />
+            <Paginator
+              page={page}
+              size={size}
+              total={explorer.count}
+              refresh={refreshPage}
+            />
           </>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FileListByAccess
+export default FileListByAccess;
 
 const NoFilesInfo = (perms) => {
   return (
-    <div className='card card-info right'>
+    <div className='card card-info'>
       <div className='card-info-attantion'>
         <IcoGet name='attantion' />
       </div>
@@ -90,27 +97,33 @@ const NoFilesInfo = (perms) => {
         Нажмите на кнопку <b>"настройки папки"</b>&nbsp;
         <IcoGet name='settings' /> для перехода в меню добавления файлов
       </div>
-      <div>В "настройках папки" так же можно поменять название папки и указать гит репозиторий</div>
+      <div>
+        В "настройках папки" так же можно поменять название папки и указать гит
+        репозиторий
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const NoFilesList = (perms) => {
   return (
-    <div className='card card-info right'>
+    <div className='card card-info'>
       <div className='card-info-attantion'>
         <IcoGet name='attantion' />
       </div>
       <h4>Список файлов пуст</h4>
-      <div>Владелец игры должен загрузить файлы, после чего они будут отображаться в списке</div>
+      <div>
+        Владелец игры должен загрузить файлы, после чего они будут отображаться
+        в списке
+      </div>
     </div>
-  )
-}
+  );
+};
 const FileItem = ({ file }) => {
   return (
     <div className='table-line m-0'>
       <div className='col col-3'>
-        {file.state === "uploaded" || file.state === "error" ? (
+        {file.state === 'uploaded' || file.state === 'error' ? (
           file.name
         ) : (
           <Link to={`/translates/${file.id}`}>
@@ -118,12 +131,12 @@ const FileItem = ({ file }) => {
           </Link>
         )}
       </div>
-      {file.state === "uploaded" ? (
+      {file.state === 'uploaded' ? (
         <div className='col col-8 row center'>
           <div>в обработке</div>
           <div className='loader-dots'>...</div>
         </div>
-      ) : file.state === "error" ? (
+      ) : file.state === 'error' ? (
         <div className='col col-8 row center'>
           <div className='color-error'>ошибка обработка файла</div>
         </div>
@@ -136,7 +149,7 @@ const FileItem = ({ file }) => {
           </div>
         </>
       )}
-      <div className='col col-1'>{file.repo_status ? "Ok" : "X"}</div>
+      <div className='col col-1'>{file.repo_status ? 'Ok' : 'X'}</div>
     </div>
-  )
-}
+  );
+};
--- front/src/Elems/PageTranslate/PageTranslateFile.js
+++ front/src/Elems/PageTranslate/PageTranslateFile.js
@@ -1,59 +1,61 @@
-import React, { useEffect, useContext, useState } from "react"
-import { useParams } from "react-router-dom"
-import AppContext from "../../context/application/appContext"
-import TranslateMenu from "./TranslateMenu"
-import TranslateMark from "./TranslateMark"
-import Paginator from "../AppComponents/Paginator"
-import Loader from "../AppComponents/Loader"
-import { DisplayImage } from "../images"
+import React, { useEffect, useContext, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import AppContext from '../../context/application/appContext';
+import TranslateMenu from './TranslateMenu';
+import TranslateMark from './TranslateMark';
+import Paginator from '../AppComponents/Paginator';
+import Loader from '../AppComponents/Loader';
+import { DisplayImage } from '../images';
 
 const PageTranslateFile = (props) => {
   // STATE
-  const { id } = useParams()
-  const { transMarkList, transFileInfo, translates } = useContext(AppContext)
-  const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState(1)
-  const [size, setSize] = useState(25)
-  const [same, setSame] = useState(false)
-  const [noSame, setNoSame] = useState(false)
-  const [like, setLike] = useState(false)
-  const [noTrans, setNoTrans] = useState(0)
-  const [langOrig, setLangOrig] = useState(null)
-  const [langTrans, setLangTrans] = useState(null)
+  const { id } = useParams();
+  const { transMarkList, transFileInfo, translates } = useContext(AppContext);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(25);
+  const [same, setSame] = useState(false);
+  const [noSame, setNoSame] = useState(false);
+  const [like, setLike] = useState(false);
+  const [noTrans, setNoTrans] = useState(0);
+  const [langOrig, setLangOrig] = useState(null);
+  const [langTrans, setLangTrans] = useState(null);
+  const [activeMark, setActiveMark] = useState(null);
 
   useEffect(() => {
-    transFileInfo(id, page, size, same, noTrans) // TODO: make like Promise
+    transFileInfo(id, page, size, same, noTrans); // TODO: make like Promise
     // eslint-disable-next-line
-  }, [id])
-
+  }, [id]);
 
   useEffect(() => {
-    if (!translates.lang_orig) return
-    if (!langOrig) setLangOrig(translates.lang_orig)
-    if (!langTrans && translates.translated_set.length) setLangTrans(translates.translated_set[0].language)
-    setLoading(false)
-  }, [translates, langOrig, langTrans])
+    if (!translates.lang_orig) return;
+    if (!langOrig) setLangOrig(translates.lang_orig);
+    if (!langTrans && translates.translated_set.length)
+      setLangTrans(translates.translated_set[0].language);
+    setLoading(false);
+  }, [translates, langOrig, langTrans]);
 
-  const fixPageNumber = (p, s, count) => (p * s > count ? Math.ceil(count / s) : p)
+  const fixPageNumber = (p, s, count) =>
+    p * s > count ? Math.ceil(count / s) : p;
 
   const refreshPage = (pageNumber, pageSize) => {
-    const fixedNumber = fixPageNumber(pageNumber, pageSize, translates.count)
-    setLoading(true)
-    setPage(fixedNumber)
-    setSize(pageSize)
+    const fixedNumber = fixPageNumber(pageNumber, pageSize, translates.count);
+    setLoading(true);
+    setPage(fixedNumber);
+    setSize(pageSize);
     transMarkList(id, fixedNumber, pageSize, same, noTrans).then(() => {
-      setLoading(false)
-    })
-  }
+      setLoading(false);
+    });
+  };
   const changeSame = () => {
-    transMarkList(id, page, size, !noSame, noTrans)
-    setNoSame(!noSame)
-  }
+    transMarkList(id, page, size, !noSame, noTrans);
+    setNoSame(!noSame);
+  };
   const changeNoTrans = () => {
-    let val = noTrans ? 0 : langTrans
-    transMarkList(id, page, size, same, val)
-    setNoTrans(val)
-  }
+    let val = noTrans ? 0 : langTrans;
+    transMarkList(id, page, size, same, val);
+    setNoTrans(val);
+  };
 
   return (
     <div className='container-fluid'>
@@ -94,22 +96,48 @@ const PageTranslateFile = (props) => {
             noTrans={noTrans}
             setNoTrans={changeNoTrans}
           />
-          <div className='expl column'>
-            <div className='scroll-y paginate'>
-              {!translates.results ? (
-                <div>NO ITEMS</div>
-              ) : (
-                translates.results.map((mark) => (
-                  <TranslateMark key={mark.id} mark={mark} langOrig={langOrig} langTrans={langTrans} same={same} />
-                ))
+          <div className='expl row'>
+            <div className='col col-7 column'>
+              <div className='table-head ml-3'>
+                <div className='col col-3'>Название</div>
+                <div className='col col-2'>Позиций</div>
+                <div className='col col-2'>Слов</div>
+                <div className='col col-3'>Прогресс перевода</div>
+                <div className='col col-2'>GIT статус</div>
+              </div>
+              <div
+                className={`scroll-y${
+                  translates.count / size > 1 ? ' paginate' : ''
+                }`}>
+                {!translates.results ? (
+                  <div>NO ITEMS</div>
+                ) : (
+                  translates.results.map((mark) => (
+                    <TranslateMark
+                      key={mark.id}
+                      mark={mark}
+                      langOrig={langOrig}
+                      langTrans={langTrans}
+                      same={same}
+                      setActive={setActiveMark}
+                    />
+                  ))
+                )}
+              </div>
+              {translates.count / size > 1 && (
+                <Paginator
+                  page={page}
+                  size={size}
+                  total={translates.count}
+                  refresh={refreshPage}
+                />
               )}
             </div>
-            <Paginator page={page} size={size} total={translates.count} refresh={refreshPage} />
           </div>
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default PageTranslateFile
+export default PageTranslateFile;
